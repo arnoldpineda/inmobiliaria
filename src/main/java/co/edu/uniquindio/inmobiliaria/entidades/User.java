84c032d4fe7a30e8d482c2134daeda9fecb4b23a
Arnold Pineda <aapinedar_1@uqvirtual.edu.co>
2023-05-16 01:23:10 -0500

    feat(User): :sparkles: Add attributes to User

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/User.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/User.java
@@ -1,4 +1,47 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @NotNull
+    @Length(max = 14)
+    @Column(nullable = false, length = 14)
+    private String dni;
+
+    @NotNull
+    @Length(max = 200)
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @NotNull
+    @Length(max = 100)
+    @Email
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
 }
