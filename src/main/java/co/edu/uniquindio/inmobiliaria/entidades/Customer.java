e71981f0e24b79b12e1479f574575f2399386a21
Arnold Pineda <aapinedar_1@uqvirtual.edu.co>
2023-05-17 22:05:17 -0500

    feat(Customer): :sparkles: Add customer entity

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Customer.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Customer.java
@@ -1,4 +1,36 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public class Customer {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@ToString(callSuper = true)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer extends User implements Serializable {
+    @Length(max = 100)
+    @NotNull
+    @Column(length = 100, nullable = false)
+    private String address;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "customer")
+    private List<Phone> phones;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private City city;
 }
