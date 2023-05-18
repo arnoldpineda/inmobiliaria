8e72d3e051b1a122c443ba0071563dba64d17d58
Arnold Pineda <aapinedar_1@uqvirtual.edu.co>
2023-05-17 22:00:11 -0500

    feat(Phone): :sparkles: Add phone entity

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Phone.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Phone.java
@@ -1,4 +1,40 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public class Phone {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Phone  implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @NotNull
+    @Length(max = 10)
+    @Column(nullable = false, length = 10)
+    private String number;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Customer customer;
 }
