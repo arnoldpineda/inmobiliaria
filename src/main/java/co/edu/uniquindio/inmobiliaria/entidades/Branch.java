f5c1605a1f361f5988561ad017af9c709a478bbd
Arnold Pineda <arnold.pineda.rojas@gmail.com>
2023-05-22 18:02:48 -0500

    feat(Branch): :sparkles: Add branch entity

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Branch.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Branch.java
@@ -1,4 +1,35 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public class Branch {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@Entity
+@ToString(callSuper = true)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Branch implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @NotNull
+    @Length(max = 200)
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @NotNull
+    @Length(max = 200)
+    @Column(nullable = false, length = 200)
+    private String address;
+
+    @NotNull
+    @Length(max = 12)
+    @Column(nullable = false, length = 12)
+    private String phone;
 }
--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Phone.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Phone.java
@@ -1,18 +1,8 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
