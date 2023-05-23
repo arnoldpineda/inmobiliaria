7a8ae94cbe07ebe99cf1ac77883154401734b619
Arnold Pineda <arnold.pineda.rojas@gmail.com>
2023-05-23 01:18:40 -0500

    feat(Position): :sparkles: Add position entity

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Position.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Position.java
@@ -1,4 +1,26 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public enum Position {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@Entity
+@Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Position implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @NotNull
+    @Length(max = 50)
+    @Column(nullable = false, length = 50)
+    private String name;
 }
