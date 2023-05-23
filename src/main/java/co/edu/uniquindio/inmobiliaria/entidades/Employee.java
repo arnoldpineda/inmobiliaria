f955358db3d433f66da4b9f67da13c0206919ede
Arnold Pineda <arnold.pineda.rojas@gmail.com>
2023-05-23 01:19:11 -0500

    feat(Employee): :sparkles: Add employee entity

--- src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Employee.java
+++ src/main/java/co/edu/uniquindio/inmobiliaria/entidades/Employee.java
@@ -1,4 +1,30 @@
 package co.edu.uniquindio.inmobiliaria.entidades;
 
-public class Employee {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@ToString(callSuper = true)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee extends User {
+  @Length(max = 14)
+  @NotNull
+  @Column(length = 14, nullable = false)
+  private String phone;
+
+  @NotNull
+  @Column(nullable = false)
+  private float salario;
+
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  private Position position;
+
+  @ManyToOne
+  @JoinColumn(nullable = false)
+  private Branch branch;
 }
